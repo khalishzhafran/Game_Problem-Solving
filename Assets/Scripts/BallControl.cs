90f0cc11593205c5afa9833d3b13f0b12c2c84df
Khalish-Litsch <khalish260303@gmail.com>
2021-12-11 16:56:21 +0800

    problem 5

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -4,19 +4,16 @@ using UnityEngine;
 
 public class BallControl : MonoBehaviour
 {
-    public float speed = 10.0f;
- 
-    private Rigidbody2D rigidBody2D;
+    public float speed;
 
-    private KeyCode upButton = KeyCode.W;
-    private KeyCode downButton = KeyCode.S;
-    private KeyCode leftButton = KeyCode.A;
-    private KeyCode rightButton = KeyCode.D;
+    Vector2 curScreenPoint;
+    Vector2 curPosition;
+    Vector2 offset;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody2D = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -27,9 +24,38 @@ public class BallControl : MonoBehaviour
 
     void MoveBall()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
-        transform.Translate(new Vector2(horizontal, vertical) * speed * Time.deltaTime);
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+
+        curPosition += offset;
+
+        //transform.position = curPosition;
+
+        //if (new Vector2(transform.position.x, transform.position.y) != curScreenPoint)
+        //{
+            transform.Translate(new Vector2(curPosition.x, curPosition.y).normalized * speed * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    transform.Translate(new Vector2(0, 0));
+        //}
     }
+
+    private void OnMouseDown()
+    {
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+    }
+
+    //private void OnMouseDrag()
+    //{
+    //    Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+    //    Vector2 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+
+    //    curPosition += offset;
+
+    //    //transform.position = curPosition;
+    //    transform.Translate(new Vector2(curPosition.x, curPosition.y) * speed * Time.deltaTime);
+    //}
 }
